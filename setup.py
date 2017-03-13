91d52329a26f102bb981cf1ffd74941a8a3a8739
Gaetano Guerriero <x.guerriero@tin.it>
2017-03-13 18:54:15 +0100

    Move jack script into bin/

--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@
 
 """Setup script for the jack module distribution."""
 
+import os
+
 from distutils.core import setup, Extension
+
+
+basepath = os.path.dirname(__file__)
 
 setup(  # Distribution meta-data
     name="jack",
@@ -12,6 +17,7 @@ setup(  # Distribution meta-data
     author_email="zarne@users.sf.net",
     url="http://www.home.unix-ag.org/arne/jack/",
 
+    scripts=(os.path.join(basepath, 'bin', 'jack'),),
     # Description of the modules and packages in the distribution
     ext_modules=[Extension('jack_cursesmodule',
                            ['cursesmodule/jack_cursesmodule.c'], libraries=["ncursesw"],
