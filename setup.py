2a54658851839992be7765f61bd104f738fa3033
Gaetano Guerriero <x.guerriero@tin.it>
2017-03-27 18:45:57 +0200

    Install jack package in setup.py instead of single modules

--- setup.py
+++ setup.py
@@ -23,15 +23,7 @@ setup(  # Distribution meta-data
                            ['cursesmodule/jack_cursesmodule.c'], libraries=["ncursesw"],
                            include_dirs=["/usr/include/ncursesw"],
                            extra_compile_args=["-Wno-strict-prototypes"])],
-
-    py_modules=['jack_CDTime', 'jack_TOC', 'jack_TOCentry', 'jack_argv',
-                'jack_checkopts', 'jack_children', 'jack_config', 'jack_constants',
-                'jack_display', 'jack_encstuff', 'jack_freedb', 'jack_functions',
-                'jack_generic', 'jack_globals', 'jack_helpers', 'jack_init', 'jack_m3u',
-                'jack_main_loop', 'jack_misc', 'jack_mp3', 'jack_playorder',
-                'jack_plugins', 'jack_prepare', 'jack_progress', 'jack_rc',
-                'jack_ripstuff', 'jack_status', 'jack_t_curses', 'jack_t_dumb', 'jack_tag',
-                'jack_targets', 'jack_term', 'jack_utils', 'jack_version', 'jack_workers']
+    packages = ['jack'],
 )
 
 print "If you have installed the modules, copy jack to some place in your $PATH,"
