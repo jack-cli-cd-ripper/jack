5f40ec13e727fa9a5b555f9f8ec8415e7bf757d9
Pim Zandbergen <pim@zandbergen.org>
2020-05-10 18:33:14 +0200

    remove my_mail from example plugins as well

--- plugins/jack_plugin_cddb.py
+++ plugins/jack_plugin_cddb.py
@@ -4,7 +4,5 @@ plugin_freedb_servers = {
     'plugin_cddb': {
         'host': "cddb.cddb.com",
         'id': "xmcd 2.6",
-        'submit_mail': "freedb-submit@freedb.org",
-        'my_mail': "default"
     },
 }
