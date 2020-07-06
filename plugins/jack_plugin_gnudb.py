b2bab5c08a82a8fe2ad63ec237693da34eb217e9
Arne Zellentin <zzarne@users.noreply.github.com>
2020-07-06 17:12:11 +0200

    Added example gnudb plugin

---
+++ plugins/jack_plugin_gnudb.py
@@ -0,0 +1,10 @@
+### jack_plugin_gnudb: example gnudb server plugin
+
+plugin_freedb_servers = {
+    'plugin_gnudb': {
+        'host': "gnudb.gnudb.org",
+        'id': "jack 3.1.1+",
+        'mail': "gnudb-submit@gnudb.org",
+        'my_mail': "default"
+    },
+}
