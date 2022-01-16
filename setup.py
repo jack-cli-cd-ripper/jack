a32f7a2fb9211071d5c30d6839c58cec8e6aa9fd
Pim Zandbergen <pim@zandbergen.org>
2022-01-16 14:01:27 +0100

    update requirements

--- setup.py
+++ setup.py
@@ -8,8 +8,12 @@ import jack.version
 PACKAGES = find_packages(exclude=find_packages(where="deprecated"))
 
 REQUIRES = [
+    #FIXME, we require either discid or libdiscid, libdiscid is preferred but requires an external C-library to be present in your distro
     'discid',
     'mutagen',
+    'pillow',
+    'requests',
+    'dateparser',
 ]
 
 setup(
