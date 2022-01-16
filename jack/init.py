c644fe5a46df0f232ca58fb3edc557226c7f8ca9
Pim Zandbergen <pim@zandbergen.org>
2022-01-16 17:52:50 +0100

    check all module dependencies

--- jack/init.py
+++ jack/init.py
@@ -23,6 +23,14 @@ import jack.discid
 from jack.generic import *
 from jack.globals import *
 
+required_modules = {
+    'libdiscid': {'site': 'https://pythonhosted.org/python-libdiscid/', 'distro-name': 'python3-libdiscid'},
+    'mutagen': {'site': 'https://mutagen.readthedocs.io/', 'distro-name': 'python3-mutagen'},
+    'pillow': {'site': 'https://pillow.readthedocs.io/', 'distro-name': 'python3-pillow'},
+    'dateparser': {'site': 'https://dateparser.readthedocs.io/', 'distro-name': 'python3-dateparser'},
+    'requests': {'site': 'https://docs.python-requests.org', 'distro-name': 'python3-requests'},
+}
+
 try:
     from fcntl import F_SETFL
 except:
@@ -34,7 +42,9 @@ except:
     from FCNTL import O_NONBLOCK
 
 if not jack.discid.init():
-    print("Please install the libdiscid module available at https://pythonhosted.org/python-libdiscid/")
+    module = 'libdiscid'
+    print("Please install the %s module described at %s .\nUse your distribution package manager where it is probably called '%s'." %
+        (module, required_modules[module]['site'], required_modules[module]['distro-name']))
     sys.exit(1)
 
 try:
@@ -44,6 +54,31 @@ try:
     import mutagen.mp4 as mp4
     import mutagen.oggvorbis as oggvorbis
 except:
-    print("Please install the Mutagen module available at")
-    print("https://mutagen.readthedocs.io/")
+    module = 'mutagen'
+    print("Please use pip to install the %s module described at %s ,\nor your distribution package manager where it is probably called '%s'." %
+        (module, required_modules[module]['site'], required_modules[module]['distro-name']))
+    sys.exit(1)
+
+try:
+    import requests
+except:
+    module = 'requests'
+    print("Please use pip to install the %s module described at %s ,\nor your distribution package manager where it is probably called '%s'." %
+        (module, required_modules[module]['site'], required_modules[module]['distro-name']))
+    sys.exit(1)
+
+try:
+    import dateparser
+except:
+    module = 'dateparser'
+    print("Please use pip to install the %s module described at %s ,\nor your distribution package manager where it is probably called '%s'." %
+        (module, required_modules[module]['site'], required_modules[module]['distro-name']))
+    sys.exit(1)
+
+try:
+    import PIL
+except:
+    module = 'pillow'
+    print("Please use pip to install the %s module described at %s ,\nor your distribution package manager where it is probably called '%s'." %
+        (module, required_modules[module]['site'], required_modules[module]['distro-name']))
     sys.exit(1)
